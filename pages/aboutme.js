ae8159c025355a1119ef899a431f2252f4d7904d
r3ndo0 <r3ndo0@gmail.com>
2022-11-20 01:48:18 +0330

    render build

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,24 +1,24 @@
 export default function aboutme() {
-    return (
-        <div className="pt-24 flex flex-col justify-center items-center w-full h-screen bg-amber-200">
-          <h1 className="text-7xl mb-12">About Me</h1>
-          <p className="max-w-[400px] text-sm">
-            Hello , I am r3ndo0 I am a self-taught web developer. Just like every
-            other new-comer to this industry, I started with HTML , CSS and a bit of
-            JS, very soon moved into ReactJS (which I am not sure if it was the best
-            decision to make). I am familiar with NextJS and its features(SSR,Folder
-            based routing etc.) I like TailwindCSS and prefer it over plain CSS. I
-            am familiar with MongoDB and CRUD operations with NodeJS. My favorite
-            part of web development so far has been creating and dealing with
-            RESTful API's. I am passionate about learning new technologies and
-            stacks as long as there is a documentation about them available. I have a
-            few freelancing projects in hand right now which are not done yet and
-            you can find them codes in my github profile(github.com/r3ndo0). And in
-            the end, I would like to thank you for putting time into taking a look
-            into my Portfolio and I want to let you know, as I have never worked in
-            a team or a company,I am OK with internships without getting paid for
-            the first one or two months. Best Regards
-          </p>
-        </div>
-      );
+  return (
+    <div className="pt-24 flex flex-col justify-center items-center w-full h-screen bg-amber-200">
+      <h1 className="text-7xl mb-12">About Me</h1>
+      <p className="max-w-[500px] text-sm">
+        Hello , I am r3ndo0 I am a self-taught web developer. Just like every
+        other new-comer to this industry, I started with HTML , CSS and a bit of
+        JS, very soon moved into ReactJS (which I am not sure if it was the best
+        decision to make). I am familiar with NextJS and its features(SSR,Folder
+        based routing etc.) I like TailwindCSS and prefer it over plain CSS. I
+        am familiar with MongoDB and CRUD operations with NodeJS. My favorite
+        part of web development so far has been creating and dealing with
+        RESTful APIs. I am passionate about learning new technologies and stacks
+        as long as there is a documentation about them available. I have a few
+        freelancing projects in hand right now which are not done yet and you
+        can find them codes in my github profile(github.com/r3ndo0). And in the
+        end, I would like to thank you for putting time into taking a look into
+        my Portfolio and I want to let you know, as I have never worked in a
+        team or a company,I am OK with internships without getting paid for the
+        first one or two months. Best Regards
+      </p>
+    </div>
+  );
 }
